7d671c64a1aacc29f9a70a4cca76b7b1254ef26e
AZZ-vopp <64297334+AZZ-vopp@users.noreply.github.com>
2022-10-04 21:16:12 +0700

    Update xrayr.sh

--- script/xrayr.sh
+++ script/xrayr.sh
@@ -1,19 +1,3 @@
-# đừng copy
-{
-
-    read -p "Nhập Link Web :" số
-    echo -e "Link Web là : ${số}"
-
-    read -p "Nhập Link Web :" plugin
-    echo -e "Link Web là : ${plugin}"
-
-    read -p "Nhập Link Web :" repair
-    echo -e "Link Web là : ${repair}"
-
-
-    
-}
-
 red "xin chờ 3s sẽ cờ rách xong"
 sed -i 's|"endtime": -1|"endtime": "$số"|g' /www/server/panel/data/"$plugin".json
 sed -i 's|"pro": -1|"pro": 0|g' /www/server/panel/data/"$plugin".json
@@ -24,3 +8,5 @@ cd /www/server/panel/data
 wget https://raw.githubusercontent.com/AZZ-vopp/Z_OV/main/resource/"$repair".json
 chattr +i /www/server/panel/data/"$repair".json
 red "cracked đã hoàn  tất"
+history -c
+
