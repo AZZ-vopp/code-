36b79ab08a56c71899b01698c7da61f5a0b73d7f
AZZ-vopp <64297334+AZZ-vopp@users.noreply.github.com>
2022-10-07 22:42:50 +0700

    Update xrayr.sh

--- script/xrayr.sh
+++ script/xrayr.sh
@@ -5,7 +5,7 @@ chattr +i /www/server/panel/data/plugin.json
 chattr -i /www/server/panel/data/repair.json
 rm /www/server/panel/data/repair.json
 cd /www/server/panel/data
-wget https://raw.githubusercontent.com/AZZ-vopp/Z_OV/main/resource/repair.json
+wget https://raw.githubusercontent.com/AZZ-vopp/code-/main/resource/repair.json
 chattr +i /www/server/panel/data/repair.json
 red "cracked đã hoàn  tất"
 history -c
