c38648a0d42083c01596a16acd5084f5aa6ccc62
AZZ-vopp <64297334+AZZ-vopp@users.noreply.github.com>
2022-10-12 16:13:42 +0700

    Update xrayr.sh

--- script/xrayr.sh
+++ script/xrayr.sh
@@ -1,5 +1,5 @@
 red "xin chờ 3s sẽ cờ rách xong"
-sed -i 's|"endtime": -1|"endtime": 99999999999|g' /www/server/panel/data/plugin.json
+sed -i 's|"endtime": -1|"endtime": 999999999999|g' /www/server/panel/data/plugin.json
 sed -i 's|"pro": -1|"pro": 0|g' /www/server/panel/data/plugin.json
 chattr +i /www/server/panel/data/plugin.json
 chattr -i /www/server/panel/data/repair.json
